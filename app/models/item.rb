29b6e154f0f4e71329f1778f9365587ac96aacd8
EtienneMangino <etiennemangino@gmail.com>
2016-10-10 04:19:20 -0700

    slack integration - <3 Adrianna

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 250 }, allow_blank: false, allow_nil: false, uniqueness: true
   validates :id, uniqueness: true
 
+  after_create :slack_post
   # validate do
   #   if content.blank? && url.blank?
   #     errors.add(:url, 'Submit a URL or Content')
@@ -20,4 +21,11 @@ class Item < ActiveRecord::Base
   scope :active, -> { where(disabled: false) }
   scope :disabled, -> { where(disabled: true) }
   scope :newest, -> { order(score: :desc) }
+
+
+  def slack_post
+    notifier = Slack::Notifier.new ENV["slack_link"]
+    post = "NEW BOOK ADDED : " + self.title + ", by @" +self.user.username + ", [Visit the Library](http://mangrove-library.herokuapp.com/items/newest)"
+    notifier.ping post
+  end
 end
